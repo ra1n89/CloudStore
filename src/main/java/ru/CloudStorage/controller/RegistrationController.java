a8e4a23593589029c79aa3210aa0ecacea5fbbb3
Ahmetzyanov <prorain88@gmail.com>
2025-02-23 17:19:15 +0500

    feat: add many code

---
+++ src/main/java/ru/CloudStorage/controller/RegistrationController.java
@@ -0,0 +1,35 @@
+package ru.CloudStorage.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.CloudStorage.service.UserService;
+
+
+@Controller
+public class RegistrationController {
+    private static final Logger log = LoggerFactory.getLogger(RegistrationController.class);
+
+    @Autowired
+    private UserService userService;
+
+
+    @GetMapping("/register")
+    public String showRegistrationForm() {
+        log.warn("showRegistrationForm  started");
+        return "register";
+    }
+
+    // Обработка данных регистрации
+    @PostMapping("/register")
+    public String registerUser(@RequestParam String username, @RequestParam String password) {
+        log.warn("Controller registerUser started", username);
+        userService.registerUser(username, password);
+        return "redirect:/login"; // Перенаправляем на страницу входа
+    }
+}
---
+++ src/test/java/ru/CloudStorage/CloudStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.CloudStorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
